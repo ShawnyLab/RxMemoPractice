c1a33c1e2506277fffd558e8aad8283bcd7f9a08
Shawn <63200799+TechD-lab@users.noreply.github.com>
2021-08-02 21:06:16 +0900

    edit Finished

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -46,6 +46,7 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
             .disposed(by: rx.disposeBag)
         
 
+        editButton.rx.action = viewModel.makeEditAction()
     }
 
 
